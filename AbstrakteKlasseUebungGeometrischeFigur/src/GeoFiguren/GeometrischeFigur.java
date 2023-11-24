2f8ae3b22a3fd6ac06dc7260bf3c880d91a3ff34
Johann Senoner <senoner.johann@proton.me>
2023-11-24 12:28:42 +0100

    fixed color

--- AbstrakteKlasseUebungGeometrischeFigur/src/GeoFiguren/GeometrischeFigur.java
+++ AbstrakteKlasseUebungGeometrischeFigur/src/GeoFiguren/GeometrischeFigur.java
@@ -1,14 +1,14 @@
 package GeoFiguren;
-
+import java.awt.*;
 public abstract class GeometrischeFigur {
-    String color;
+    private Color farbe;
 
-    public String getColor() {
-        return color;
+    public Color getColor() {
+        return farbe;
     }
 
-    public void setColor(String color) {
-        this.color = color;
+    public void setColor(Color color) {
+        this.farbe = color;
     }
 
     public abstract double berechneFlaeche();
